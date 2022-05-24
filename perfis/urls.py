961c9d71b2ef900686e2ae0a4dc2ed772d955052
sdavi3489 <sdavi3489@gmail.com>
2022-05-24 12:50:51 -0300

    Commit do modulo 8

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from perfis import views
 
 urlpatterns = [
-    path ('', views.index, name='index'),
-    path('perfis', views.exibir, name='exibir'),
+	path('', views.index, name='index'),
+	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+	# re_path(r'^perfis/\d+$', views.exibir, name='perfis')
+	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+	return render(request, 'index.html',{'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
-def exibir(request):
-    return render(request, 'perfil.html')
+def exibir(request, perfil_id):	
+	perfil = Perfil.objects.get(id=perfil_id)
+	return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request)})
+
+def convidar(request, perfil_id):
+	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+	perfil_logado = get_perfil_logado(request)
+	perfil_logado.convidar(perfil_a_convidar)
+	return redirect('index')
+
+def get_perfil_logado(request):
+	return Perfil.objects.get(id=1)
